37fa0225dd2cb14858446fe795f5fdae5b9cd9b8
shahin <shahin.mozaffarie@gmail.com>
2024-08-10 10:48:55 +0430

    update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// no need for hiding this variables
+// because row level security is activated
+// only reading data is accessible
+
+const supabaseUrl = "https://okoaezanvsztnvblbfsf.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rb2FlemFudnN6dG52YmxiZnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE1NTAwMDQsImV4cCI6MjAzNzEyNjAwNH0.uHF3Skc6-Zrjl_dafUu94DaFOg3ziT4rwrgAXrHXs1w"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
